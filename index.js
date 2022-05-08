ca0f3c53edb7f1a723620fcd2c43eb4cab5f22c6
Slobodan Krstic <slobodan.krstich@gmail.com>
2022-05-08 14:15:04 +0200

    Added Stock API

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+
+const request = require('request');
+
 const PORT = process.env.PORT || 5000;
 
 const { engine } = require('express-handlebars');
+
+//  API KEY pk_8a651a9cd09c4dc2a5bbefde92cccd37
+//creat call AP func.
+function call_api(finshedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_8a651a9cd09c4dc2a5bbefde92cccd37', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); };
+        if (res.statusCode === 200) {
+            finshedAPI(body);
+        };
+
+    });
+};
+
 
 
 //Sets our app to use the handlebars engine
@@ -18,11 +34,18 @@ app.set('views', './views');
 const otherstuff = 'Hello this is other stuff...';
 //set handlebar routes
 app.get('/', (req, res) => {
-    res.render('home', {
-        stuff: otherstuff
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        });
+
     });
 });
 
+//create about page route
+app.get('/about.html', (req, res) => {
+    res.render('about');
+});
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
